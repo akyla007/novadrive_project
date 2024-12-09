bdb7a7d76db582d9d7ba35abebc74abc5058fe1b
Akyla <akylaaquino@hotmail.com>
2024-12-09 13:13:27 -0300

    feat: fixing outliers and categoric columns

--- const.py
+++ const.py
@@ -1,3 +1,8 @@
 from pathlib import Path
 
-CONSULTA_SQL = Path('./sql/SELECT_DATA.sql')
+
+def get_sql(path: Path) -> str:
+    with open(path, 'r') as file:
+        return file.read()
+
+CONSULTA_SQL = get_sql(Path('sql/SELECT_DATA.sql'))
